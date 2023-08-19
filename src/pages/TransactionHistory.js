8e6b08edeab47af0f1268afbb34cb91e125fab39
Aman Rohi <aman.rohi369@gmail.com>
2023-08-20 02:29:04 +0530

    last commit

--- src/pages/TransactionHistory.js
+++ src/pages/TransactionHistory.js
@@ -50,8 +50,8 @@ function TransactionHistory() {
           <Navbar />
           <div className="w-[90%] shadow-md shadow-gray-500">
             <div className="flex justify-between bg-indigo-400 px-4 py-3 shadow-sm shadow-gray-300 rounded-md ">
-              <p className=" text-white text-[20px]">Tomato</p>
-              <p className=" text-white text-[20px]">tomato@gmail.com</p>
+              <p className=" text-white text-[20px]">Virat Kohli</p>
+              <p className=" text-white text-[20px]">virat@gmail.com</p>
             </div>
             <div className="flex flex-col gap-4  p-12 overflow-scroll h-full">
               {response.map((transaction) => (
